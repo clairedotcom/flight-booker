a934ae8eb366fd870f57834528c49d5aad5d138e
clairedotcom <claireacarson@gmail.com>
2022-02-18 18:19:57 -0500

    Add and populate flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,12 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @date_options = Flight.all.map { |f| [format_flight_date(f.start), f.id] }
+  end
+
+  private
+
+  def format_flight_date(date)
+    date.strftime("%m/%d/%Y %H:%M")
   end
 end
