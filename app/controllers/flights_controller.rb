f0d5c9b921823c913cc39728900e8a746e61de1c
clairedotcom <claireacarson@gmail.com>
2022-02-19 17:04:48 -0500

    Refactor flights#index view to only show dates with flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |a| [a.code, a.id] }
-    @date_options = Flight.all.map { |f| [format_flight_date(f.start), f.id] }
+    # @search_date = Date.new(params[])
+    # @search_results = 
   end
 
   private
 
-  def format_flight_date(date)
-    date.strftime("%m/%d/%Y %H:%M")
-  end
+  # def flight_params
+    # params.require(:flight).permit(:origin_airport_id, :destination_airport_id, :start, :duration)
+  # end
 end
