f37a0129b729dec82afbf4f0f3c5895153d7e0d7
clairedotcom <claireacarson@gmail.com>
2022-02-21 17:18:59 -0500

    Update flights controller and flights#index view to show search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    # @search_date = Date.new(params[])
-    # @search_results = 
+    @search_date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    @search_results = Flight.where("origin_airport_id = ? AND destination_airport_id = ?", params[:origin_airport].to_i, params[:destination_airport].to_i)
   end
 
   private
 
-  # def flight_params
-    # params.require(:flight).permit(:origin_airport_id, :destination_airport_id, :start, :duration)
-  # end
+  def flight_params
+    params.require(:flight).permit(:origin_airport_id, :destination_airport_id, :start, :duration)
+  end
 end
