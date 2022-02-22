93bb42956410b5d6ba3c80d59d5fa7c131b7a231
clairedotcom <claireacarson@gmail.com>
2022-02-22 08:41:03 -0500

    Create search results partial

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,9 @@
 class FlightsController < ApplicationController
   def index
-    @search_date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    @search_results = Flight.where("origin_airport_id = ? AND destination_airport_id = ?", params[:origin_airport].to_i, params[:destination_airport].to_i)
+    if params.key?(:year) && params.key?(:origin_airport)
+      @search_date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      @search_results = Flight.where("origin_airport_id = ? AND destination_airport_id = ?", params[:origin_airport].to_i, params[:destination_airport].to_i)
+    end
   end
 
   private
