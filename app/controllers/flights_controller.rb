236631f04ef35ce303a150ed1e15817fecb905aa
clairedotcom <claireacarson@gmail.com>
2022-02-23 12:37:19 -0500

    Create Booking model, create associations, and migrate db

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     if params.key?(:year) && params.key?(:origin_airport)
       @search_date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-      @search_results = Flight.where("origin_airport_id = ? AND destination_airport_id = ?", params[:origin_airport].to_i, params[:destination_airport].to_i)
+      @search_results = Flight.where("origin_airport_id = ? AND destination_airport_id = ? AND departure_date = ?", params[:origin_airport].to_i, params[:destination_airport].to_i, @search_date)
     end
   end
 
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :origin_airport, class_name: "Airport"
   belongs_to :destination_airport, class_name: "Airport"
+  has_many :bookings
 
   def info
     "#{origin_airport.code} #{arrow.encode} #{destination_airport.code}  |  #{departure_time}  |  #{duration.to_f} hours"
