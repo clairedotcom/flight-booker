fd5acd3a640f36a9994c0556c490e5b85e3f4dca
clairedotcom <claireacarson@gmail.com>
2022-02-24 12:55:14 -0500

    Create Booking#new, Booking#show and update Bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight])
+    params[:passengers].to_i.times { @booking.passengers.build }
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :airports
   resources :flights
-  resources :bookings, only: [:new]
+  resources :bookings, only: [:new, :create, :show]
 
   root 'flights#index'
 end
