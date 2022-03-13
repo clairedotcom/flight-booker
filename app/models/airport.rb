1467cc61a9eefbe35ed0e3fc8e6539d6a20939b6
clairedotcom <claireacarson@gmail.com>
2022-03-12 21:08:37 -0500

    Move form select options from index view to Flight and Airport models

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,8 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: "Flight", foreign_key: "origin_airport_id"
   has_many :arriving_flights, class_name: "Flight", foreign_key: "destination_airport_id"
+
+  def self.options_for_select
+    Airport.all.map { |a| [a.code, a.id] }
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,18 @@ class Flight < ApplicationRecord
   belongs_to :destination_airport, class_name: "Airport"
   has_many :bookings
 
+  def self.month_options
+    Flight.all.map { |f| f.departure_date.month }.uniq
+  end
+
+  def self.day_options
+    Flight.all.map { |f| f.departure_date.day }.uniq
+  end
+
+  def self.year_options
+    Flight.all.map { |f| f.departure_date.year }.uniq
+  end
+
   def info
     "#{origin_airport.code} #{arrow.encode} #{destination_airport.code}  |  #{departure_time}  |  #{duration.to_f} hours"
   end
