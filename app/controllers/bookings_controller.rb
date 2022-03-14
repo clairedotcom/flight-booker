9c9390aecac704a6b55acb96e24503d407012815
clairedotcom <claireacarson@gmail.com>
2022-03-14 12:56:35 -0400

    Add booking confirmation email feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,10 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
 
     if @booking.save
+      @booking.passengers.each do |passenger|
+        PassengerMailer.with(passenger: passenger).confirmation_email.deliver_now
+      end
+
       redirect_to @booking
     else
       render :new
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'from@example.com'
+
+  def confirmation_email
+    @passenger = params[:passenger]
+
+     mail(to: @passenger.email, subject: "Flight Booking Confirmation")
+  end
 end
